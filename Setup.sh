69371f55e33d26f5fabe66fbc8291bc8f2bf509b
jkk111 <kellyj58@tcd.ie>
2018-04-10 15:42:03 -0700

    Started work on server setup script

---
+++ Setup.sh
@@ -0,0 +1,18 @@
+mkdir -p /app/socket
+mkdir -p /app/ssl
+chmod -R 666 /app/ssl
+chown -R root:root /app/ssl
+
+sudo apt update
+sudo apt install curl git
+
+export DEBIAN_FRONTEND="noninteractive"
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt update
+sudo apt-get install -q -y nodejs nginx mysql-server openssh-server
+
+mysqladmin -uroot password
+
+git clone https://github.com/jkk111/ApplicationManager /app/manager
+
