a9001477f7a3ccd1335bb06781d642df97cf2df2
jkk111 <kellyj58@tcd.ie>
2018-04-12 12:39:45 -0700

    Additional Work On Scripts

--- BetterSetup.sh
+++ BetterSetup.sh
@@ -4,48 +4,20 @@ if [[ $EUID != 0 ]]; then
   exit
 fi
 
-function password() {
-  local need_password=1
-  local bad_attempt=0
-  local confirm=""
+# Get Credentials With A bash script so we can setup without additional prompts
+. SetupCreds.sh
 
-  while [[ $need_password -eq 1 ]]
-  do
-    if [[ $bad_attempt -eq 1 ]]; then
-      echo "Passwords Must Match"
-    fi
-    echo "Please Enter Password For $1"
-    echo -n "Password: "
-    read -s password
-    echo "$2 ${!2}"
-
-    echo -n "Confirm: "
-    read -s confirm
-    echo
-
-    if [[ $password = $confirm ]]; then
-      need_password=0
-    else
-      bad_attempt=1
-    fi
-    eval "${2}=$password"
-  done
-}
-
-echo "Starting Setup"
-
-echo "First we need to get some credentials"
-password MySql mysql_password
-password "Secret Manager" secret_password
-
+# Recurring Bug, fails to apt update because of this repo
 sed -i '/deb cdrom/s/^/#/g' /etc/apt/sources.list
 
 mkdir -p /app/socket
 mkdir -p /app/ssl
 mkdir -p /app/ca
 
-chmod -R 666 /app/ssl
+chown -R 600 /app/ca
+chmod -R 600 /app/ssl
 chown -R root:root /app/ssl
+chown -R root:root /app/ca
 
 sudo apt update
 sudo apt install curl git
@@ -57,4 +29,4 @@ apt-get -q -y install nodejs nginx mysql-server openssh-server openssl
 
 mysqladmin -uroot password $mysql_password
 
-git clone https://github.com/jkk111/ApplicationManager /app/managers
+git clone https://github.com/jkk111/ApplicationManager /app/manager
---
+++ SetupCreds.sh
@@ -0,0 +1,81 @@
+function password() {
+  local need_password=1
+  local bad_attempt=0
+  local confirm=""
+
+  while [[ $need_password -eq 1 ]]
+  do
+    if [[ $bad_attempt -eq 1 ]]; then
+      echo "Passwords Must Match"
+    fi
+    echo "Please Enter Password For $1"
+    echo -n "Password: "
+    read -s password
+    echo "$2 ${!2}"
+
+    echo -n "Confirm: "
+    read -s confirm
+    echo
+
+    if [[ $password = $confirm ]]; then
+      need_password=0
+    else
+      bad_attempt=1
+    fi
+    eval "${2}=$password"
+  done
+}
+
+function json_object() {
+  local str=""
+  local i=0
+
+  for j in "$@"
+  do
+    if [[ $i -gt 0 ]]; then
+      str="${str},"
+    fi
+
+    str="$str$j"
+    i=$i+1
+  done
+
+  echo "{ $str }"
+}
+
+function json_pair() {
+  echo "\"$1\": \"$2\""
+}
+
+function json_secret() {
+  local key=$(printf '%q' $1)
+  local value=$(printf '%q' $2)
+  local pass=$(printf '%q' $3)
+  local file=$(printf '%q' $4)
+
+  key=$(json_pair key $key)
+  value=$(json_pair value $value)
+  pass=$(json_pair pass $pass)
+  file=$(json_pair store $file)
+
+  local result=$(json_object "$key" "$value" "$pass" "$file")
+
+  echo "$result"
+}
+
+echo "First we need to configure your Keyring"
+password Keyring keyring_pass
+
+echo "Next we need a password for the Credentials Store"
+password credentials credentials_pass
+
+echo "Next we need a MySql root Password"
+password Mysql mysql_pass
+
+credentials_secret=$(json_secret credentials "${credentials_pass}" "${keyring_pass}" node_app_keyring)
+mysql_user_secret=$(json_secret db_user root "${credentials_pass}" "credentials")
+mysql_pass_secret=$(json_secret db_pass "${mysql_pass}" "${credentials_pass}" "credentials")
+
+echo $credentials_secret | secrets
+echo $mysql_user_secret | secrets
+echo $mysql_pass_secret | secrets
